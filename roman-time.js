c7a43fa615dfaa8cf470c51d5aa7e5e4dfb5ea39
avega2991 <avega2991@github.com>
2015-10-04 01:06:28 +0500

    cosmetic_fixes

--- roman-time.js
+++ roman-time.js
@@ -1,60 +1,66 @@
 var hours = process.argv[2];
 var minutes = process.argv[3];
 
+var ints    = [50, 40, 10, 9, 5, 4, 1],
+    romans 	= ["L", "XL", "X", "IX", "V", "IV", "I"],
+    asciiDict = {"L" : [ "(_)      ",
+                         "(_)      ",
+                         "(_)      ",
+                         "(_)      ",
+                         "(_)(_)(_)" ],
 
-var ints 	= [50, 40, 10, 9, 5, 4, 1],
-    romans 	= ['L', 'XL', 'X', 'IX', 'V', 'IV', 'I'],
-    asciiDict = {'L' : [ "(_)      ",
-						 "(_)      ",
-						 "(_)      ",
-						 "(_)      ",
-						 "(_)(_)(_)" ],
+                 "X" : [ "(_)   (_)", 
+                         " (_) (_) ", 
+                         "   (_)   ",
+                         " (_) (_) ", 
+                         "(_)   (_)"  ],
 
-				 'X' : [ "(_)   (_)", 
-				 		 " (_) (_) ", 
-				 		 "   (_)   ",
-				 		 " (_) (_) ", 
-				 		 "(_)   (_)"  ],
+                 "V" : [ "(_)     (_)",
+                         " (_)   (_) ", 
+                         "  (_) (_)  ", 
+                         "   (_ _)   ",
+                         "    (_)    "  ],
 
-				 'V' : [ "(_)     (_)",
-				 		 " (_)   (_) ", 
-				 		 "  (_) (_)  ", 
-				 		 "   (_ _)   ",
-				 		 "    (_)    "  ],
+                 "I" : [ " (_) ", 
+                         " (_) ", 
+                         " (_) ",
+                         " (_) ",
+                         " (_) " ],
 
-				 'I' : [ " (_) ", 
-				 		 " (_) ", 
-				 		 " (_) ",
-				 		 " (_) ",
-				 		 " (_) " ],
+                 "N" : [ "(_)     (_)", 
+                         "(_ _)   (_)",
+                         "(_) (_) (_)",
+                         "(_)   (_ _)",
+                         "(_)     (_)", ],
 
-				 'N' : [ "(_)     (_)", 
-				 		 "(_ _)   (_)",
-				 		 "(_) (_) (_)",
-				 		 "(_)   (_ _)",
-				 		 "(_)     (_)", ],
-
-				 ':' : [ "  (_)(_)  ",
-				 		 "  (_)(_)  ",
-				 		 "          ",
-				 		 "  (_)(_)  ",
-				 		 "  (_)(_)  "]};
+                 ":" : [ "   (_)(_)   ",
+                         "   (_)(_)   ",
+                         "            ",
+                         "   (_)(_)   ",
+                         "   (_)(_)   "]};
 
 function isValid(hours, minutes)
 {
-	return (hours >= 24 || minutes >= 60) ? false : true;
+    var hoursInt = parseInt(hours);
+    var minutesInt = parseInt(minutes);
+
+    var hValid = (hoursInt >= 0 && hoursInt < 24);
+    var mValid = (minutesInt >= 0 && minutesInt < 60);
+
+    return (hValid && mValid);
 }
 
 function intToRoman(number)
 {
-	var result = '';
-    for (var i = 0; i < ints.length; i++) 
+    if (number == 0)
     {
-    	if (number == 0)
-    	{
-    		result += 'N';
-    		break;
-    	}
+        return 'N';
+    }
+
+    var result = '';
+    var intsLen = ints.length;
+    for (var i = 0; i < intsLen; ++i) 
+    {
         while (number >= ints[i]) {
             result += romans[i];
             number -= ints[i];
@@ -66,34 +72,29 @@ function intToRoman(number)
 
 function romanToGraph(roman)
 {
-	var result = '';
-	for (var row = 0; row < 5; row++)
-	{
-		for(var col = 0; col < roman.length; col++)
-		{
-			result += asciiDict[ roman.charAt(col) ][row];
-		}
-		result += '\n';
-	}
+    var result = '';
+    for (var row = 0; row < 5; ++row)
+    {
+        var romanLen = roman.length;
+        for(var col = 0; col < romanLen; ++col)
+        {
+            result += asciiDict[ roman[col] ][row];
+        }
+        result += '\n';
+    }
 
-	return result;
+    return result;
 }
 
-
-function main()
+if (!isValid(hours, minutes))
 {
-	if (!isValid(hours, minutes))
-	{
-		console.log("Time is incorrect");
-		return;
-	}
-
-	var hoursRoman = intToRoman(hours);
-	var minutesRoman = intToRoman(minutes);
-
-	var time = hoursRoman + ':' + minutesRoman;
-
-	console.log(romanToGraph(time));
+    console.log("Time is incorrect");
+    return;
 }
 
-main();
+var hoursRoman = intToRoman(hours);
+var minutesRoman = intToRoman(minutes);
+
+var time = hoursRoman + ":" + minutesRoman;
+
+console.log(romanToGraph(time));
